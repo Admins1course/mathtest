21751db7b58866706a142b7c2b60ae31a735426f
Larchenko Maxim <grouppi1course@gmail.com>
2020-05-01 17:41:17 +0300

    ограничил доступ к коду

--- searchForPiece.php
+++ searchForPiece.php
@@ -1,10 +1,10 @@
 <?php
 include 'includes/db.inc.php';
-
-$sql="SELECT id, name, surname, root FROM `users` 
-		WHERE name LIKE '".strtolower($_POST['searchValue'])."%' OR
-			  surname LIKE '".strtolower($_POST['searchValue'])."%'";
-		//".strtolower($_POST['searchValue'])."
-$result=$pdo->query($sql);
-$jsonResult=json_encode($result->fetchAll(PDO::FETCH_ASSOC));
-echo $jsonResult;		
+if ($_POST){
+	$sql="SELECT id, name, surname, root FROM `users` 
+			WHERE name LIKE '".strtolower($_POST['searchValue'])."%' OR
+				  surname LIKE '".strtolower($_POST['searchValue'])."%'";
+	$result=$pdo->query($sql);
+	$jsonResult=json_encode($result->fetchAll(PDO::FETCH_ASSOC));
+	echo $jsonResult;
+}		
