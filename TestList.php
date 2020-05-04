0af65b94c7486d4d8def749989171de449b59813
D3n4ik <grouppi1course@gmail.com>
2020-05-04 20:23:49 +0300

    Создал 2 include файла (1 это Html элементы навигационного меню и 2 скрипты к ним)

--- TestList.php
+++ TestList.php
@@ -16,45 +16,8 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 	<link rel="stylesheet" href="style/CsslistTest.css?<?=time()?>" type="text/css">
 	<script type="text/javascript" src="http://code.jquery.com/jquery-latest.js"></script>
-	<script type="text/javascript">
-		$(document).ready(function(){
-			$('.exit_menu').click(function(){
-				$('.exit_menu_body').slideToggle(500);
-			});
-		});
-	</script>
-		<script>
-	$(document).ready(function($) {
-	$('.load_avatar_open').click(function() {
-		$('.load_avatar_fade').fadeIn();
-		return false;
-	});	
 	
-	$('.load_avatar_close').click(function() {
-		$(this).parents('.load_avatar_fade').fadeOut();
-		return false;
-	});		
- 
-	$(document).keydown(function(e) {
-		if (e.keyCode === 27) {
-			e.stopPropagation();
-			$('.load_avatar_fade').fadeOut();
-		}
-	});
-	
-	
-});
-	$(function(){
-		$(window).scroll(function() {
-			if($(this).scrollTop() >= 501) {
-				$('#left_block').addClass('stickytop');
-			}
-			else{
-				$('#left_block').removeClass('stickytop');
-			}
-		});
-	});
-</script>	
+<?php include 'includes/script_for_nav_menu.php';?>	
 
 </head>
 <body style="height: 2000px;">
@@ -138,101 +101,7 @@
 		
 
 	
-<div id="nav_menu">
-				<nav id="menu1">
-				 <ul>
-				  <li><a href="index.php">Главная</a></li>
-				  <li><a href="#m2">О нас</a></li>
-				  <li><a href="#m3">Тесты</a>
-				   <ul>
-				    <li><a href="TestList.php">Каталог тестов</a></li>
-					<?php if(isset($_SESSION['data-user']['root'])&&($_SESSION['data-user']['root']=="студент")){?>
-						<li><a href="#m3_4">Статистика</a></li>
-						<li><a href="#m3_5">Пройти тест по приглашению</a></li>
-				    <?php }else if(isset($_SESSION['data-user']['root'])&&($_SESSION['data-user']['root']=="преподаватель")){?>
-						<li><a href="#m3_3">Мой каталог</a></li>
-						<li><a href="createtest.html.php">Создать тест</a></li>
-						<li><a href="#m3_5">Создать приглашение</a></li>
-					<?php } ?>
-				   </ul>
-				  </li>
-				  <li><a href="#m4">Новости</a></li>
-				  <li><a href="#m5">Контакты</a></li>
-				  <li>
-				  	<div class="open_notifications_body">
-					  	<a href="#m5">Оповещения
-						  	<div class="notifications">
-
-								<div class="notific_num">
-									<p>99+</p>
-								</div>
-						  	</div>	
-						</a>
-
-						<div class="notifications_body" style="display: none;">
-							<div class="notifications_bar">
-								<p class="text_notifications_bar">Привет</p>
-							</div>
-						</div>
-					</div>
-
-				  </li>
-				 </ul>
-				 
-				</nav><!--menu1-->
-				<div class="profile">
-					<?php
-						if (isset($_SESSION['data-user']['name'])&&isset($_SESSION['data-user']["surname"])):?>
-							
-							<div class="load_avatar_fade">
-								<div class="load_avatar">
-									<div class="preview_image_div">
-										<div class="preview_image" id="img-preview" >
-											
-										</div>
-										
-									</div>
-									<div class="load_image">
-									  <label for="custom-file-upload" class="filupp">
-									    <span class="filupp-file-name js-value" >Загрузить файл</span>
-									    <input type="file" name="attachment-file " value="1"  id="custom-file-upload"  >
-									  </label>
-									</div>
-		
-									
-									<a class="load_avatar_close" href="">X</a>
-								</div>
-
-								
-							</div>
-							<div class="profile_avatar load_avatar_open">
-								<p class="plus_photo">+</p>
-							</div>
-							<div class="user_profile_title">
-						
-								<p class="exit_menu"><?=htmlspecialchars($_SESSION['data-user']['name'])." <br /> ".htmlspecialchars($_SESSION['data-user']['surname'])?></p>
-							</div>
-							<div class="exit_menu_body" style="display:none">
-
-								<div class="exit_menu_elements">
-									<p class="exit_menu_stat">Роль: <?php $_SESSION['data-user']['root'];?></p>
-								</div>
-								
-								<div class="exit_title exit_menu_elements">
-
-									<p><a href="vyhod.php">Выход</a></p>
-								</div>
-							</div>
-						<?php 
-						else:?>
-							<div class="enter_site_btn">
-								<a  href="nevEnter.html.php">Войти</a>
-							</div>
-					<?php endif?>
-				</div>
-			</div>
-	
-		</div>
+<?php include 'includes/nav_menu.php';?>
 		
 	
 	
